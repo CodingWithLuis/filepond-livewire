bc5626b051187b60061f8996eefeaac9807078d6
CodingWithLuis <codewithluis11@gmail.com>
2021-11-24 18:44:46 -0600

    Install Filepond, Livewire, Create new component

---
+++ app/Http/Livewire/Video/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Video;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.video.create');
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,10 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import * as FilePond from 'filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+
+
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
